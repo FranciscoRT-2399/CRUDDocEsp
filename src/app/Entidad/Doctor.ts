2160b98ab3ac42410d293fce94bf372369a3170d
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-21 23:48:42 -0600

    Edicion de componentes y Entidades

---
+++ src/app/Entidad/Doctor.ts
@@ -0,0 +1,8 @@
+import { Especialidades } from "./Especialidad";
+
+export class Doctores{
+	idDoctor !: number;
+	nombre !: string;
+	especialidad !: string;
+	espId !: Especialidades;
+}
---
+++ src/app/Entidad/Especialidad.ts
@@ -0,0 +1,4 @@
+export class Especialidades{
+	idEsp !: number;
+	nombre !: string;
+}
